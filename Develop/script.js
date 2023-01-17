5a8c67b696f21d602d3029975bdb1cce38c108b0
JordAnne <jbethune37@gmail.com>
2023-01-16 22:18:00 -0500

    added parameters to the pass word using upper case, lower case, number and special characters. also added if (so when something is selected theres an alert to guide user

--- Develop/script.js
+++ Develop/script.js
@@ -2,9 +2,21 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  
+  var length = window.prompt("How long do you want your password?");
+    if(length <=7 || length >=128) {
+      alert("Password cant be shorter than 8 characters or longer than 128 characters");
+    }
+    alert("First step done! YAY!")
 
-}
+  var upperCase = confirm("Click OK if you want upper case letters in your password");
+  var lowerCase = confirm("Click OK if you want lower case letters in your password");
+  var numbers = confirm("Click OK if you want numbers in your password");
+  var special = confirm("Click OK if you want special characters in your password");
+    
+    if(upperCase === false && lowerCase === false && special ===false && numbers === false) {
+      alert("INVALID")
+    }
+  }
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
