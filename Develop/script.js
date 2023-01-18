0c688072d64480471e9607a031af482c7facc92f
JordAnne <jbethune37@gmail.com>
2023-01-17 21:05:58 -0500

    changed the if to while so the user cant move on before setting the parameters correctly. add the letters, numbers and special characters that will randomly generate

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var special = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "?"]
+
+
 
 function generatePassword() {
   var length = window.prompt("How long do you want your password?");
-    if(length <=7 || length >=128) {
+    while(length <=7 || length >=128) {
       alert("Password cant be shorter than 8 characters or longer than 128 characters");
+      var length = window.prompt("How long do you want your password?")
     }
     alert("First step done! YAY!")
 
@@ -12,10 +19,16 @@ function generatePassword() {
   var lowerCase = confirm("Click OK if you want lower case letters in your password");
   var numbers = confirm("Click OK if you want numbers in your password");
   var special = confirm("Click OK if you want special characters in your password");
-    
-    if(upperCase === false && lowerCase === false && special ===false && numbers === false) {
-      alert("INVALID")
+
+    while(upperCase === false && lowerCase === false && special ===false && numbers === false) {
+      alert("INVALID");
+      var upperCase = confirm("Click OK if you want upper case letters in your password");
+      var lowerCase = confirm("Click OK if you want lower case letters in your password");
+      var numbers = confirm("Click OK if you want numbers in your password");
+      var special = confirm("Click OK if you want special characters in your password");
     }
+
+
   }
 // Write password to the #password input
 function writePassword() {
